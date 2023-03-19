1951a8684ca425766ca6bc8fb23f52370c9f31e7
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2023-03-19 17:55:16 +0100

    stats

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import People from "./assets/careerPeople.png";
 import Stats from "./assets/frame.png";
-import Background from "./assets/statsBackground.png"
+import Background from "./assets/statsBackground.png";
 
 const App = () => {
   return (
@@ -45,9 +45,30 @@ const App = () => {
         </div>
       </div>
       {/* stats */}
-      <div className="h-[786px] flex items-center text-white relative">
-        <div className="w-[694px] h-full bg-[url('./assets/statsBackground.png')] bg-cover bg-center">
-          your stat to make a move
+      <div className="h-[786px] flex items-center relative">
+        <div className="w-[694px] h-full bg-[url('./assets/statsBackground.png')] bg-cover bg-center flex items-center justify-center">
+          <div className=" w-[570px]">
+            <h1 className="font-bold text-5xl mb-4">USE YOUR STATS TO MAKE MOVES</h1>
+            <p className="text-xl font-normal">
+              Figma files can also be downloaded from our store. These files
+              include the latest Figma features and are synced with the Webflow
+              template.
+              <br />
+              <br />
+              Figma files can also be downloaded from our store. These files
+              include the latest Figma features and are synced
+              <br />
+              <br />
+              Figma files can also be downloaded from our store. These files
+              include the latest Figma features and are synced with the Webflow
+              template.
+              <br />
+              <br />
+            </p>
+            <p>
+              Try it now <span>arrow here</span>
+            </p>
+          </div>
         </div>
         <div className="relative pt-32 p-20 bg-black h-full">
           <div className="w-[558px] h-[512px] bg-[#1A1D1F] absolute left-24 top-24 flex items-center justify-center">
